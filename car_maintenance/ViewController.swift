98674bc36ac3b3bc53e4f475298d2f880b06d989
Paul Kao <paulkao94@gmail.com>
2017-10-06 11:58:42 -0700

    initialize project

---
+++ car_maintenance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  car_maintenance
+//
+//  Created by Paul Kao on 2017-10-06.
+//  Copyright © 2017 Paul Kao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
