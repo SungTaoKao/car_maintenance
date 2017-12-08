3d4da8ef31e23352e22310d0a9f0c94b98d31559
Paul Kao <paulkao94@gmail.com>
2017-12-08 01:47:11 -0800

    misch

---
+++ car_maintenance/InfoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  InfoViewController.swift
+//  car_maintenance
+//
+//  Created by Paul Kao on 2017-12-08.
+//  Copyright © 2017 Paul Kao. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loadForm(){
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
